d5769f68033ba3345933ef66c5c3f8075b039417
Mohamed Tajjiou <shutsugan@gmail.com>
2020-06-07 15:42:30 +0100

    test(app): clean up empty files

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import { MockedProvider } from "@apollo/client/testing";
 
 import Header from "./Header";
 
 test("renders learn react link", () => {
   const { getByText } = render(<Header />);
-  const linkElement = getByText(/Loading/i);
+  const linkElement = getByText(/MNGR/i);
   expect(linkElement).toBeInTheDocument();
 });
